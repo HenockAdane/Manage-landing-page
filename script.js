fecb53cbbd50d57e3666a15a28189425159f7266
HenockAdane <65575319+HenockAdane@users.noreply.github.com>
2020-08-03 20:24:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,43 @@
-let phoneNav = document.querySelector(".phone-nav");
+let menu = document.querySelector(".menu");
 let nav = document.querySelector("nav");
 
-phoneNav.addEventListener("click", function(e){
-    e.preventDefault();
-    if (nav.style.display === "block"){
-        nav.style.display = "none";
-    }
-    else{
-        nav.style.display = "block"
-    }
+menu.addEventListener("click", ()=>{
+  if(nav.style.display === "flex"){
+    menu.classList.toggle("open");
+    nav.style.display = "none"
+  }
+  
+  else{
+    menu.classList.toggle("open")
+    nav.style.display = "flex";
+    gsap.from(".odd", {duration:1, x:"-100%", duration: 0.3})
+    gsap.from(".even", {duration:1, x:"100%", duration: 0.3})
+    // gsap.from("nav", {duration:1, y:"-100%", duration: 0.3})
+  }
+
 })
 
 
-
-
-let simpleBtn = document.querySelector(".simple");
-let speedyBtn = document.querySelector(".speedy");
-let easyBtn = document.querySelector(".easy")
-
-let tabOne = document.querySelector(".one");
-let tabTwo = document.querySelector(".two");
-let tabThree = document.querySelector(".three");
-
-simpleBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    simpleBtn.style.textDecoration = "underline";
-    speedyBtn.style.textDecoration = "none";
-    easyBtn.style.textDecoration = "none";
-    tabOne.style.display = "flex";
-    tabTwo.style.display = "none";
-    tabThree.style.display = "none";
-})
-
-speedyBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    speedyBtn.style.textDecoration = "underline";
-    simpleBtn.style.textDecoration = "none";
-    easyBtn.style.textDecoration = "none";
-    tabTwo.style.display = "flex";
-    tabOne.style.display = "none";
-    tabThree.style.display = "none";
-})
-
-easyBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    easyBtn.style.textDecoration = "underline";
-    simpleBtn.style.textDecoration = "none";
-    speedyBtn.style.textDecoration = "none";
-    tabThree.style.display = "flex";
-    tabOne.style.display = "none";
-    tabTwo.style.display = "none";
-})
-
-let dropdown = document.querySelectorAll(".dropdown");
-dropdown.forEach(function(a){
-    a.addEventListener("click",function(e){
-        e.preventDefault();
-        if(e.target.parentNode.classList.contains("dropdown-Link")){
-            console.log("true")
-            for (let b of a.children){
-                if (b.classList.contains("dropdown-content")){
-                    if(b.style.display === "block"){
-                        b.style.display = "none"
-                    }
-
-                    else{b.style.display = "block"}
-                }
-            }
-                /*if (b.classList.contains("dropdown-content")){
-                    b.style.display = "block"
-                }*/
-            
-        }
-        else{
-            console.log("false")
-        }
-    })       
-})
 
 let timeline = gsap.timeline({defaults:{duration:1}})
 timeline
 .from("header", {y:"-100%"})
-.from(".bookmark-logo", {opacity:0},"<")
+.from(".logo1", {opacity:0},"<")
 .from("nav", {opacity:0},"<")
 .from("main", {x:"-100%"},"<")
-.from(".features", {x:"100%"},"<")
-.from(".tabs", {x:"-100%"},"<")
-.from(".extension", {x:"100%"},"<")
-.from(".questions-container", {x:"-100%"},"<")
-.from("footer", {y:"100%"},"<")
+.from(".difference", {x:"100%"},"<")
+.from(".review-section", {x:"-100%"},"<")
+.from(".footer1", {x:"100%"},"<")
+
+let navLinks = document.querySelectorAll(".nav-links");
+
+
+
+// navLinks.forEach((a, index)=>{
+   
+
+//  if (a.classList.contains("odd"))
+
+
+// })
+// console.log(1)
